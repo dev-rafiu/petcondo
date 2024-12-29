7d41fab0b7da57d943546872374dce9df0706406
Rafiw Idrissu <dev.rafiu@gmail.com>
2024-12-29 13:35:55 +0000

    feat: setup opengraph

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,9 +1,8 @@
 const DEPLOYED_URL = process.env.NEXT_PUBLIC_URL;
 
 export const siteConfig = {
-  name: 'Insured By STK',
-  description:
-    'Protect what matters most with Insured By STK. From comprehensive car insurance and flexible car rentals to speeding ticket assistance and temporary cover, we’ve got you covered. Get affordable, reliable solutions tailored to your needs.',
+  name: 'PetCondo',
+  description: 'Everything you need to care for your pet, all in one app',
   URL: DEPLOYED_URL
     ? `https://${DEPLOYED_URL}`
     : `http://localhost:${process.env.PORT || 3000}`,
