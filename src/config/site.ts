d930fd719814be67f39d70a9b408cf0b395df63d
Rafiw Idrissu <dev.rafiu@gmail.com>
2024-12-30 13:28:25 +0000

    update .env

--- src/config/site.ts
+++ src/config/site.ts
@@ -2,7 +2,7 @@ const DEPLOYED_URL = process.env.NEXT_PUBLIC_URL;
 
 export const siteConfig = {
   name: 'PetCondo',
-  description: 'Everything you need to care for your pet, all in one app.',
+  description: 'Everything you need to care for your pet, all in one app',
   URL: DEPLOYED_URL
     ? `https://${DEPLOYED_URL}`
     : `http://localhost:${process.env.PORT || 3000}`,
